015c320ca9b9303d2beda809c23486ff8f0e453a
zhouzhiguan <zhiqian112488@163.com>
2022-08-11 11:06:15 +0800

    first commit

---
+++ Sources/CatFramework.xcframework/ios-arm64_x86_64-simulator/CatFramework.framework/Headers/CatFramework.h
@@ -0,0 +1,18 @@
+//
+//  CatFramework.h
+//  CatFramework
+//
+//  Created by 周志官 on 2022/8/11.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CatFramework.
+FOUNDATION_EXPORT double CatFrameworkVersionNumber;
+
+//! Project version string for CatFramework.
+FOUNDATION_EXPORT const unsigned char CatFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CatFramework/PublicHeader.h>
+
+
--- Sources/CatPackage/CatPackage.swift
+++
@@ -1,6 +0,0 @@
-public struct CatPackage {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
